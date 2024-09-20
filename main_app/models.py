63ba16860dd5dfc9dbc19bf1c7595c437d11e8fc
eternal <149954940+EternalGeniuz@users.noreply.github.com>
2024-09-20 12:16:20 +0300

    +postgres

--- main_app/models.py
+++ main_app/models.py
@@ -24,12 +24,13 @@ class Set(models.Model):
     price = models.IntegerField
     time = models.IntegerField
     services = models.ManyToManyField(Service)
+    image = models.ImageField(upload_to='sets_image/', null=True, blank=True)
 
 
 class Record(models.Model):
     date = models.DateField
     start_time = models.TimeField
     end_time = models.TimeField
-    master_id = models.ForeignKey(Master)
+    master_id = models.ForeignKey(Master, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     set_id = models.ForeignKey(Set, on_delete=models.PROTECT)
